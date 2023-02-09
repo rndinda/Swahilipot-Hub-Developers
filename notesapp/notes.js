026da6ac7b32576a26fd52d6ff17eaf828788bae
Ndinda rose <rndinda73@gmail.com>
2023-02-09 12:23:50 +0300

    Notes app

--- notesapp/notes.js
+++ notesapp/notes.js
@@ -1,18 +1,54 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded())
 const port = 3000
 
+// endpoints to html
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile("pages/index.html", { root: __dirname })
 })
 
 app.get('/login', (req, res) => {
-    res.send('login!')
-  })
+  res.sendFile("pages/login.html", { root: __dirname })
+})
 
-  app.get('/Signup', (req, res) => {
-    res.send('Signup!')
-  })
+app.get('/signup', (req, res) => {
+  res.sendFile("pages/signup.html", { root: __dirname })
+})
+
+// endpoints to server for APIs 
+
+app.post('/getnotes', (req, res) => {
+  const { userToken } = req.body
+  res.sendFile("pages/signup.html", { root: __dirname })
+})
+
+app.post('/login', (req, res) => {
+  const { userToken } = req.body
+  res.sendFile("pages/signup.html", { root: __dirname })
+})
+
+app.post('/signup', (req, res) => {
+  const { userToken } = req.body
+  res.sendFile("pages/signup.html", { root: __dirname })
+})
+
+app.post('/addnote', (req, res) => {
+  const { userToken } = req.body
+  res.sendFile("pages/signup.html", { root: __dirname })
+})
+
+app.post('/editnote', (req, res) => {
+  const { userToken } = req.body
+  res.sendFile("pages/signup.html", { root: __dirname })
+})
+
+app.post('/deletenote', (req, res) => {
+  const { userToken } = req.body
+  res.sendFile("pages/signup.html", { root: __dirname })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
