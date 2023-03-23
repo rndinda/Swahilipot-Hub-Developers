81ef41033d8d04affd2051c4ad91a1b1d5d8cef6
Ndinda rose <rndinda73@gmail.com>
2023-03-23 12:21:18 +0300

    Express

--- nodejs/expressjs/1-express.js
+++ nodejs/expressjs/1-express.js
@@ -19,35 +19,66 @@ const http = require('http')
 
 // REQUEST.
 
-const server = http.createServer((req,res)=>{
-    // console.log(req.method)
-    // console.log(req.url)
+// const server = http.createServer((req,res)=>{
+//     // console.log(req.method)
+//     // console.log(req.url)
 
-    const url = req.url;
+//     const url = req.url;
 
-    // home page
-    if (url === '/'){
-        res.writeHead(200,{'content-type':'text/html'})
-        res.write('<h1> Heey , na hii home page')
-        res.end()
- }
+//     // home page
+//     if (url === '/'){
+//         res.writeHead(200,{'content-type':'text/html'})
+//         res.write('<h1> Heey , na hii home page </h1>')
+//         res.end()
+//  }
 
    
-    else if( url ==='/about'){
-        res.writeHead(200,{'content-type':'text/html'})
-        res.write('<h1> Huku ni about tu </h1>')
+//     else if( url ==='/about'){
+//         res.writeHead(200,{'content-type':'text/html'})
+//         res.write('<h1> Huku ni about tu </h1>')
+//         res.end()
+
+//     }
+
+//     else{
+//         res.writeHead(400,{'content-type':'text/html'})
+//         res.write('<h1>Umepotea msee</h1>')
+//         res.end()
+//     }
+
+
+//     // res.writeHead(200,{'contnent-type':'txt/html'})
+//     // res.write('<h1>Good morning People,</h1>')
+//     //  res.end()
+// }).listen(5001)
+
+const {readFileSync} = require('fs')
+
+const firstPage = readFileSync('./express.html')
+
+const server = http.createServer((req,res)=>{
+    const url =req.url
+
+    // Home page
+    if(url === '/'){
+        res.writeHead(200,{'content-type': 'text/html'})
+        res.write(firstPage)
+        res.write(`<h1>Hey ,good day </h1>`)
+        res.end()
+    }
+//   About page
+    else if(url === '/about'){
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.write(`<h1>we work on the following</h1>`)
         res.end()
 
     }
-
+// 404 error
     else{
-        res.writeHead(400,{'content-type':'text/html'})
-        res.write('<h1>Umepotea msee</h1>')
+        res.writeHead(400, {'content-type':'text/html'})
+        res.write(`<h1> Huku hakuna kitu </h1>`)
         res.end()
+
     }
 
-
-    // res.writeHead(200,{'contnent-type':'txt/html'})
-    // res.write('<h1>Good morning People,</h1>')
-    //  res.end()
 }).listen(5001)
